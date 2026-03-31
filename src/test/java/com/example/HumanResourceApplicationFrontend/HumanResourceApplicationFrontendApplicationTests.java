70f1edb6e955f47de0e45bb0214c1607dbf7e0e7
Prathmesh2011 <prathmeshrewatkar2011@gmail.com>
2026-03-31 11:22:14 +0530

    initial commit

---
+++ src/test/java/com/example/HumanResourceApplicationFrontend/HumanResourceApplicationFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HumanResourceApplicationFrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HumanResourceApplicationFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
