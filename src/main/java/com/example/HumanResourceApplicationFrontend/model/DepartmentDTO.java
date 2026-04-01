56b542b9cd3ed77ec3ac141f8e82984216c83633
Samiddha024 <samiddha.raich@gmail.com>
2026-04-01 22:44:21 +0530

    Department UI

--- src/main/java/com/example/HumanResourceApplicationFrontend/model/DepartmentDTO.java
+++ src/main/java/com/example/HumanResourceApplicationFrontend/model/DepartmentDTO.java
@@ -1,7 +1,37 @@
+//package com.example.HumanResourceApplicationFrontend.model;
+//
+//import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+//
+//@JsonIgnoreProperties(ignoreUnknown = true)
+//public class DepartmentDTO {
+//    private Integer departmentId;
+//    private String departmentName;
+//    private LocationDTO location;
+//    private EmployeeDTO manager;
+//
+//    // Getters and Setters
+//    public Integer getDepartmentId() { return departmentId; }
+//    public void setDepartmentId(Integer departmentId) { this.departmentId = departmentId; }
+//
+//    public String getDepartmentName() { return departmentName; }
+//    public void setDepartmentName(String departmentName) { this.departmentName = departmentName; }
+//
+//    public LocationDTO getLocation() { return location; }
+//    public void setLocation(LocationDTO location) { this.location = location; }
+//
+//    public EmployeeDTO getManager() { return manager; }
+//    public void setManager(EmployeeDTO manager) { this.manager = manager; }
+//}
+
+
+
 package com.example.HumanResourceApplicationFrontend.model;
 
+import com.example.HumanResourceApplicationFrontend.model.EmployeeDTO;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
 
+@Data
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class DepartmentDTO {
     private Integer departmentId;
@@ -9,16 +39,21 @@ public class DepartmentDTO {
     private LocationDTO location;
     private EmployeeDTO manager;
 
-    // Getters and Setters
-    public Integer getDepartmentId() { return departmentId; }
-    public void setDepartmentId(Integer departmentId) { this.departmentId = departmentId; }
+    @Data
+    @JsonIgnoreProperties(ignoreUnknown = true)
+    public static class LocationDTO {
+        private Integer locationId;
+        private String city;
+        private String streetAddress;
+        private String stateProvince;
+        private String postalCode;
+    }
+//
+//    @Data
+//    public static class ManagerDTO {
+//        private String firstName;
+//        private String lastName;
+//        private String email;
+//    }
 
-    public String getDepartmentName() { return departmentName; }
-    public void setDepartmentName(String departmentName) { this.departmentName = departmentName; }
-
-    public LocationDTO getLocation() { return location; }
-    public void setLocation(LocationDTO location) { this.location = location; }
-
-    public EmployeeDTO getManager() { return manager; }
-    public void setManager(EmployeeDTO manager) { this.manager = manager; }
 }
