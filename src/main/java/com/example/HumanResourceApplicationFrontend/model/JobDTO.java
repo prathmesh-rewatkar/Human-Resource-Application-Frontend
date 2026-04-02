e7d0588ef6fd0627b53b2555fc1fdc859fe8da0b
Prathmesh2011 <prathmeshrewatkar2011@gmail.com>
2026-04-02 13:00:00 +0530

    add changes

--- src/main/java/com/example/HumanResourceApplicationFrontend/model/JobDTO.java
+++ src/main/java/com/example/HumanResourceApplicationFrontend/model/JobDTO.java
@@ -1,7 +1,10 @@
 package com.example.HumanResourceApplicationFrontend.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
 import java.math.BigDecimal;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class JobDTO {
 
     private String jobId;
